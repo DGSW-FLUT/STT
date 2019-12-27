0f320369738a6a5c6cad9058551a478c1eb9d448
윤재상 <apple01644@hotmail.com>
2019-12-27 13:23:39 +0900

    ADD Server and ADD Client Example

--- index.js
+++ index.js
@@ -1,7 +1,43 @@
-require('dotenv').config();
+//require('dotenv').config();
 const recorder = require('node-record-lpcm16');
+//Change args from "node-record-lpcm16/recordes\sox.js"
+/*
+let args = [
+  '--no-show-progress', // show no progress
+  '-t', options.audioType,
+  '-d',
+  '--rate', options.sampleRate, // sample rate
+  '--channels', options.channels, // channels
+  '--encoding', 'signed-integer', // sample encoding
+  '--bits', '16', // precision (bits)
+  '--type', 'raw', // audio type
+  '-' // pipe
+]
+*/
 const speech = require('@google-cloud/speech');
+//1. Install Google Cloud SDK
+//2. ADD Evrionment PATH GOOGLE_APPLICATION_CREDENTIALS
+/*
+https://cloud.google.com/docs/authentication/getting-started
+*/
 const client = new speech.SpeechClient();
+
+const app = require('express')();
+const http = require('http').createServer(app);
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/client.html');
+});
+http.listen(80, () => {
+  console.log('listening on *:3000');
+});
+
+const io = require('socket.io')(3000);
+io.on('connection', socket => { 
+  socket.on('abc', data => {
+
+  });
+});
 
 const request = {
   config: {
@@ -13,54 +49,25 @@ const request = {
 };
 
 const recognizeStream = client
-  .streamingRecognize(request)
-  .on('error', console.error)
-  .on('data', data => {
-    let searchTitle = data.results[0].alternatives[0];
+.streamingRecognize(request)
+.on('error', e => {console.log(recognizeStream); console.error(e);})
+.on('data', data => {
+  if (data.results[0] && data.results[0].alternatives[0])
+  {
+    const text = data.results[0].alternatives[0].transcript;
+    io.emit('transcript', text);
+    console.log(text);
+  }
+});
 
-      process.stdout.write(
-        data.results[0] && data.results[0].alternatives[0]
-          ? `Transcription: ${data.results[0].alternatives[0].transcript}\n`
-          : `\n\nReached transcription time limit, press Ctrl+C\n`
-      )
-
-    searchYoutube(searchTitle);
-    }
-  );
-
-recorder
-  .record({
-    sampleRateHertz: 16000,
-    threshold: 0,
-    device: null,
-    verbose: false,
-    recordProgram: 'rec',
-    silence: '10.0',
+recorder.record({
+  sampleRateHertz: 16000,
+  threshold: 0.5,
+  device: null,
+  verbose: false,
+  recordProgram: 'rec',
+  silence: '10.0',
+  audioType: 'waveaudio'
 }).stream().on('error', console.error).pipe(recognizeStream);
 
 console.log('Listening, press Ctrl+C to stop.');
-
-async function searchYoutube(data) {
-  const YouTube = require('youtube-node');
-  const open = require('open');
-  const { YoutubeApiKey: key } = process.env;
-
-
-  const youtube = new YouTube();
-  
-  youtube.setKey(key);
-  youtube.addParam('type', 'video');
-
-  youtube.search(`${data.transcript}`, 5, async(err, result) => {
-    if(err) {
-      console.log(err);
-    } else {
-      console.log(JSON.stringify(result, null, 5));
-
-      let { videoId } = result.items[0].id;
-      let url = "https://www.youtube.com/watch?v=" + videoId;
-
-      await open(url, {app:'chrome'});
-    }
-  })
-}
